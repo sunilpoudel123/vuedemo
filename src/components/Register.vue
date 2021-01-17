174f9057df2d200cd98beade6115bb9315611179
Proha <pnlwlust@users.noreply.github.com>
2021-01-17 20:36:47 +0545

    Register changes

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,47 +1,48 @@
 <template>
-    <div class="vue-tempalte">
+  <div class="vue-tempalte">
     <form>
-        <h3>Sign Up</h3>
+      <h3>Sign Up</h3>
 
-<div class="form-group">
-    <label>Full Name</label>
-<input type="text" class="form-control form-control-lg"/>
-    </div>
+      <div class="form-group">
+        <label>Full Name</label>
+        <input type="text" class="form-control form-control-lg"/>
+      </div>
 
-    <div class="form-group">
-    <label>Email address</label>
-<input type="email" class="form-control form-control-lg" />
-    </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control form-control-lg" />
+      </div>
 
-    <div class="form-group">
-    <label>Password</label>
-    <input type="password" class="form-control form-control-lg" />
-    </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
 
-    <div class="form-group">
-    <label>Repeat Password</label>
-    <input type="password" class="form-control form-control-lg" />
-    </div>
+      <div class="form-group">
+        <label>Repeat Password</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
 
-    <div class="form-group">
-    <label>Invitation Code</label>
-    <input type="password" class="form-control form-control-lg" />
-    </div>
+      <div class="form-group">
+        <label>Invitation Code</label>
+        <input type="password" class="form-control form-control-lg" />
+      </div>
 
-    <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
+      <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
 
-<p class="forgot-password text-right">
-    Already have an account
-<router-link :to="{name: 'login'}">sign in?</router-link>
-    </p>
+      <p class="forgot-password text-right">
+        Already have an account
+        <router-link to="/login">sign in?</router-link>
+      </p>
     </form>
-    </div>
-    </template>
+  </div>
+</template>
 
-    <script>
+<script>
+
 export default {
-    data() {
-        return {}
-    }
+  data() {
+    return {}
+  }
 }
 </script>
