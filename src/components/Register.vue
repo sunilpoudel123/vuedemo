276d706cae2df6c25287f6ac614690d86aa98e37
sunil poudel <sunil.poudel@itech101.global>
2021-01-17 19:55:52 +0545

    wip

---
+++ src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="vue-tempalte">
+    <form>
+        <h3>Sign Up</h3>
+
+<div class="form-group">
+    <label>Full Name</label>
+<input type="text" class="form-control form-control-lg"/>
+    </div>
+
+    <div class="form-group">
+    <label>Email address</label>
+<input type="email" class="form-control form-control-lg" />
+    </div>
+
+    <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control form-control-lg" />
+    </div>
+
+    <div class="form-group">
+    <label>Repeat Password</label>
+    <input type="password" class="form-control form-control-lg" />
+    </div>
+
+    <div class="form-group">
+    <label>Invitation Code</label>
+    <input type="password" class="form-control form-control-lg" />
+    </div>
+
+    <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
+
+<p class="forgot-password text-right">
+    Already have an account
+<router-link :to="{name: 'login'}">sign in?</router-link>
+    </p>
+    </form>
+    </div>
+    </template>
+
+    <script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    // {
+    //     path: '/',
+    //     name: 'Home',
+    //     component: Home
+    // },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
